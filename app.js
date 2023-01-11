26bacc48ea27e0bf409df0b7176ae8ce7b756e02
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-11 14:34:18 -0500

    app js basic express app added

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('E-Commerce API');
+const express = require("express");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+require("express-async-errors");
+
+// other packages
+const morgan = require("morgan");
+
+// connect DB
+
+const connectDB = require("./db/connect");
+
+// middleware
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => res.status(200).send("E-commerce-API!"));
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`listening on port ${port}!`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
